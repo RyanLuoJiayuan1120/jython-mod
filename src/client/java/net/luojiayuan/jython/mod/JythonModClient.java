5cac68a3bb321e554d26135fcbb1d6ba2abcef56
RyanLuoJiayuan1120 <luojiayuan20111120@outlook.com>
2026-03-07 18:46:23 +0800

    。 .

--- src/client/java/net/luojiayuan/jython/mod/JythonModClient.java
+++ src/client/java/net/luojiayuan/jython/mod/JythonModClient.java
@@ -4,18 +4,33 @@ import java.io.InputStream;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.loader.api.FabricLoader;
+import net.luojiayuan.jython.mod.config.ModConfig;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import com.mojang.blaze3d.platform.InputConstants;
+import org.lwjgl.glfw.GLFW;
 import org.python.util.PythonInterpreter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
-import net.luojiayuan.jython.mod.config.ModConfig;
 
 public class JythonModClient implements ClientModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("jython-mod-client");
 
+	// // 注册按键绑定 - 使用 Minecraft 内置的 MISC 分类
+	// public static final KeyMapping OPEN_CONFIG_KEY = KeyBindingHelper.registerKeyBinding(
+	// 	new KeyMapping(
+	// 		"key.jython-mod.open_config",
+	// 		InputConstants.Type.KEYSYM,
+	// 		GLFW.GLFW_KEY_G,
+	// 		KeyMapping.Category.MISC
+	// 	)
+	// );
 	@Override
 	public void onInitializeClient() {
 		// 确保配置已初始化
@@ -76,5 +91,13 @@ public class JythonModClient implements ClientModInitializer {
 		} catch (Exception e) {
 			LOGGER.error("Runtime Error in running client modules: " + e.getMessage(), e);
 		}
+
+		// 注册按键监听
+		// ClientTickEvents.END_CLIENT_TICK.register(client -> {
+		// 	while (OPEN_CONFIG_KEY.consumeClick()) {
+		// 		// 打开配置屏幕
+		// 		client.setScreen(new net.luojiayuan.jython.mod.config.ModConfigScreen(client.screen));
+		// 	}
+		// });
 	}
 }
